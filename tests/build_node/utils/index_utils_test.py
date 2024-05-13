0c2d96c5f3069f55219feb18f0e1987cf8f51a1a
Igor Sudak <isudak@cloudlinux.com>
2024-05-13 13:04:46 +0000

    Added tests for index_utils and proc_utils
    Resolves: AlmaLinux/build-system/issues/279

---
+++ tests/build_node/utils/index_utils_test.py
@@ -0,0 +1,14 @@
+import os
+from build_node.utils import index_utils
+
+
+def test_index_utils(request):
+    pkg_path = os.path.join(
+        os.path.dirname(os.path.dirname(os.path.dirname(request.node.fspath))),
+        'test_repo/a-1.0-1.el6.noarch.rpm'
+    )
+    pkg_data = index_utils.extract_metadata(pkg_path)
+    # Check a couple of fields
+    assert pkg_data['arch'] == 'noarch'
+    assert pkg_data['name'] == 'a'
+    assert pkg_data['sourcerpm'] == 'a-1.0-1.el6.src.rpm'
---
+++ tests/build_node/utils/proc_utils_test.py
@@ -0,0 +1,15 @@
+import errno
+import os
+from unittest.mock import patch
+
+from build_node.utils import proc_utils
+
+
+def test_proc_utils():
+    proc_utils.get_current_thread_ident()
+    assert proc_utils.is_pid_exists(os.getpid())
+
+    e = OSError()
+    e.errno = errno.ESRCH
+    with patch('build_node.utils.proc_utils.os.kill', side_effect=e):
+        assert not proc_utils.is_pid_exists(1)
